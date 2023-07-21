04728e785088ab1c6e27e4a889b93583fff00047
PQerror <140090820+PQerror@users.noreply.github.com>
2023-07-22 01:02:28 +1200

    Delete script.lua

--- script.lua
+++
@@ -1,9 +0,0 @@
-local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/PQerror/whitelist/main/clientids"))() -- hwids
-local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
-for i,v in pairs(HWIDTable) do
-    if v == HWID then
-    wait(0)
-else
-  game.Players.LocalPlayer:Kick("not wled")
-end
-end
